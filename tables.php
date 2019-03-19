d95ac98ecb995985c14b81daf70c134137179834
Amondt <mondt.alexande>
2019-03-19 20:20:35 +0100

    exe 3 tables done

--- tables.php
+++ tables.php
@@ -5,6 +5,15 @@
     $num = 24;
     $isMan = false;
 
+    $month = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august',
+            'september', 'october', 'december'];
+    
+    $departements = ['01' => 'Ain', '03' => 'Allier', '07' => 'Ardèche', '15' => 'Cantal', 
+                    '26' => 'Drôme', '38' => 'Isère', '42' => 'Loire', '43' => 'Haute-Loire',
+                    '63' => 'Puy-de-Dôme', '69' => 'Rhône', '73' => 'Savoie', '74' => 'Haute-Savoie'];
+    $departements['57'] = 'Moselle';
+
+    // var_dump($departements);
     // Exe 1
     function vrai() {
         return true;
@@ -18,7 +27,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Learn PHP - Functions</title>
+    <title>Learn PHP - Tables</title>
     <style>
         * {
             margin: 0;
@@ -47,14 +56,19 @@
     </style>
 </head>
 <body>
-    <h1>Functions</h1>
+    <h1>Tables</h1>
 
-    <!-- exercice 1 -->
-    <h2>Exercice 1</h2>
+    <!-- exercice 1 / 2 -->
+    <h2>Exercice 1 / 2</h2>
     <div class="my-text">
-        <p><?php if (vrai()) echo vrai() ?></p>
+        <p><?php echo $month[2] . '<br>' . $month[5] ?></p>
     </div>
 
+    <!-- exercice 3 -->
+    <h2>Exercice 3</h2>
+    <div class="my-text">
+        <p><?php foreach ($departements as $i => $value) { echo 'Le département ' . $value . ' a le numéro ' . $i . '<br>'; } ?></p>
+    </div>
 
 </body>
 </html>
